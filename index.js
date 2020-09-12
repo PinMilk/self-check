2411dc0b2f6762129639dcae5b7d729ef076dcc5
Hanbyeol Jo <hanbyeul04@kakao.com>
2020-09-12 12:12:48 +0900

    commit

--- index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var self_check_1 = require("./src/self-check");
-Object.defineProperty(exports, "SelfChecker", { enumerable: true, get: function () { return self_check_1.SelfChecker; } });
