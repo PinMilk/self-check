986f032cf76da6aff7ceb10b307e3a806cacc1ec
Hanbyeol Jo <hanbyeul04@kakao.com>
2020-10-27 20:44:28 +0900

    commit

--- src/searchSchool.ts
+++ src/searchSchool.ts
@@ -1,56 +1,69 @@
-import Axios from 'axios';
+import Axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
 
 export class SchoolFinder {
     protected name: string;
     protected region: string;
     protected kind: string;
-    protected schoolCode: any;
-    protected schoolKind: any;
-
+    protected schoolCode: any = {
+        "서울": "01",
+        "부산": "02",
+        "대구": "03",
+        "인천": "04",
+        "광주": "05",
+        "대전": "06",
+        "울산": "07",
+        "세종": "08",
+        "경기": "10",
+        "강원": "11",
+        "충북": "12",
+        "충남": "13",
+        "전북": "14",
+        "전남": "15",
+        "경북": "16",
+        "경남": "17",
+        "제주": "18"
+    };
+    protected schoolKind: any = {
+        "유치원": "1",
+        "초등학교": "2",
+        "중학교": "3",
+        "고등학교": "4",
+        "특수학교": "5"
+    };
+    /**
+     * 
+     * @param name School name
+     * @param region School region
+     * @param kind School kind
+     */
     constructor(name: string, region: string, kind: string) {
         this.name = name;
         this.region = region;
         this.kind = kind;
-        this.schoolCode = {
-            "서울": "01",
-            "부산": "02",
-            "대구": "03",
-            "인천": "04",
-            "광주": "05",
-            "대전": "06",
-            "울산": "07",
-            "세종": "08",
-            "경기": "10",
-            "강원": "11",
-            "충북": "12",
-            "충남": "13",
-            "전북": "14",
-            "전남": "15",
-            "경북": "16",
-            "경남": "17",
-            "제주": "18"
-        };
-        this.schoolKind = {
-            "유치원": "1",
-            "초등학교": "2",
-            "중학교": "3",
-            "고등학교": "4",
-            "특수학교": "5"
-        };
     }
-
+    /**
+     * 
+     * @param url request url
+     * @param config request config
+     */
+    private async request(url: string, config: AxiosRequestConfig): Promise<AxiosResponse<any>> {
+        return await Axios.get(url, config);
+    }
+    /** */
     public async find() {
-        const result: any = (await Axios.get(`https://hcs.eduro.go.kr/school?orgName=${encodeURI(this.name)}&lctnScCode=${this.schoolCode[this.region]}&schulCrseScCode=${this.schoolKind[this.kind]}`, {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X)\
+        const url: string = `https://hcs.eduro.go.kr/v2/searchSchool?lctnScCode=${this.schoolCode[this.region]}&schulCrseScCode=${this.schoolKind[this.kind]}&orgName=${encodeURI(this.name)}`;
+        const headers: any = {
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X)\
                     AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
-            }
-        })).data.schulList[0];
+        };
+        const response: AxiosResponse = await this.request(url, { headers });
+        const document: any = response.data;
+        const result: any = document.schulList[0];
         return result;
     }
-
-    public async getCode() {
-        const result: string = (await this.find()).orgCode;
-        return result;
+    /** */
+    public async getCode(): Promise<string> {
+        const schoolCode: string = (await this.find()).orgCode;
+        return schoolCode;
     }
 }
--- src/self-check.ts
+++ src/self-check.ts
@@ -3,7 +3,29 @@ import { SchoolFinder } from "./searchSchool";
 import Axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export class SelfChecker {
-    protected url: any;
+    protected url: any = {
+        "서울": "https://senhcs.eduro.go.kr",
+        "부산": "https://penhcs.eduro.go.kr",
+        "대구": "https://dgehcs.eduro.go.kr",
+        "인천": "https://icehcs.eduro.go.kr",
+        "광주": "https://genhcs.eduro.go.kr",
+        "대전": "https://djehcs.eduro.go.kr",
+        "울산": "https://usehcs.eduro.go.kr",
+        "세종": "https://sjehcs.eduro.go.kr",
+        "경기": "https://goehcs.eduro.go.kr",
+        "강원": "https://kwehcs.eduro.go.kr",
+        "충북": "https://cbehcs.eduro.go.kr",
+        "충남": "https://cnehcs.eduro.go.kr",
+        "전북": "https://jbehcs.eduro.go.kr",
+        "전남": "https://jnehcs.eduro.go.kr",
+        "경북": "https://gbehcs.eduro.go.kr",
+        "경남": "https://gnehcs.eduro.go.kr",
+        "제주": "https://jjehcs.eduro.go.kr",
+        "path": [
+            "/loginwithschool",
+            "/registerServey"
+        ]
+    };
     protected name: string;
     protected birthday: string;
     protected school: string;
@@ -22,29 +44,6 @@ export class SelfChecker {
      * @returns void
      */
     constructor(name: string, birthday: string, school: string, region: string, kind: string, schoolCode: string = '') {
-        this.url = {
-            "서울": "https://senhcs.eduro.go.kr",
-            "부산": "https://penhcs.eduro.go.kr",
-            "대구": "https://dgehcs.eduro.go.kr",
-            "인천": "https://icehcs.eduro.go.kr",
-            "광주": "https://genhcs.eduro.go.kr",
-            "대전": "https://djehcs.eduro.go.kr",
-            "울산": "https://usehcs.eduro.go.kr",
-            "세종": "https://sjehcs.eduro.go.kr",
-            "경기": "https://goehcs.eduro.go.kr",
-            "강원": "https://kwehcs.eduro.go.kr",
-            "충북": "https://cbehcs.eduro.go.kr",
-            "충남": "https://cnehcs.eduro.go.kr",
-            "전북": "https://jbehcs.eduro.go.kr",
-            "전남": "https://jnehcs.eduro.go.kr",
-            "경북": "https://gbehcs.eduro.go.kr",
-            "경남": "https://gnehcs.eduro.go.kr",
-            "제주": "https://jjehcs.eduro.go.kr",
-            "path": [
-                "/loginwithschool",
-                "/registerServey"
-            ]
-        };
         this.name = name;
         this.birthday = birthday;
         this.school = school;
@@ -52,26 +51,22 @@ export class SelfChecker {
         this.kind = kind;
         this.schoolCode = schoolCode;
     }
-
-    private encrypt(str: string): string {
-        const pem: string = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA81dCnCKt0NVH7j5Oh2+S
-GgEU0aqi5u6sYXemouJWXOlZO3jqDsHYM1qfEjVvCOmeoMNFXYSXdNhflU7mjWP8
-jWUmkYIQ8o3FGqMzsMTNxr+bAp0cULWu9eYmycjJwWIxxB7vUwvpEUNicgW7v5nC
-wmF5HS33Hmn7yDzcfjfBs99K5xJEppHG0qc+q3YXxxPpwZNIRFn0Wtxt0Muh1U8a
-vvWyw03uQ/wMBnzhwUC8T4G5NclLEWzOQExbQ4oDlZBv8BM/WxxuOyu0I8bDUDdu
-tJOfREYRZBlazFHvRKNNQQD2qDfjRz484uFs7b5nykjaMB9k/EJAuHjJzGs9MMMW
-tQIDAQAB
------END PUBLIC KEY-----`;
-        /*  I got this PEM key with this code
-            Buffer.from(
-                Forge.util.hexToBytes(
-                    "30820122300d06092a864886f70d01010105000382010f003082010a0282010100f357429c22add0d547ee3e4e876f921a0114d1aaa2e6eeac6177a6a2e2565ce9593b78ea0ec1d8335a9f12356f08e99ea0c3455d849774d85f954ee68d63fc8d6526918210f28dc51aa333b0c4cdc6bf9b029d1c50b5aef5e626c9c8c9c16231c41eef530be91143627205bbbf99c2c261791d2df71e69fbc83cdc7e37c1b3df4ae71244a691c6d2a73eab7617c713e9c193484459f45adc6dd0cba1d54f1abef5b2c34dee43fc0c067ce1c140bc4f81b935c94b116cce404c5b438a0395906ff0133f5b1c6e3b2bb423c6c350376eb4939f44461164195acc51ef44a34d4100f6a837e3473e3ce2e16cedbe67ca48da301f64fc4240b878c9cc6b3d30c316b50203010001"
-                ), "binary"
-            ).toString("base64")
-        */
+    /**
+     * 
+     * @param message an message to be encrypted
+     */
+    private encrypt(message: string): string {
+        const pem: string = '-----BEGIN PUBLIC KEY-----' + '\n' +
+            'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA81dCnCKt0NVH7j5Oh2+S' + '\n' +
+            'GgEU0aqi5u6sYXemouJWXOlZO3jqDsHYM1qfEjVvCOmeoMNFXYSXdNhflU7mjWP8' + '\n' +
+            'jWUmkYIQ8o3FGqMzsMTNxr+bAp0cULWu9eYmycjJwWIxxB7vUwvpEUNicgW7v5nC' + '\n' +
+            'wmF5HS33Hmn7yDzcfjfBs99K5xJEppHG0qc+q3YXxxPpwZNIRFn0Wtxt0Muh1U8a' + '\n' +
+            'vvWyw03uQ/wMBnzhwUC8T4G5NclLEWzOQExbQ4oDlZBv8BM/WxxuOyu0I8bDUDdu' + '\n' +
+            'tJOfREYRZBlazFHvRKNNQQD2qDfjRz484uFs7b5nykjaMB9k/EJAuHjJzGs9MMMW' + '\n' +
+            'tQIDAQAB' + '\n' +
+            '-----END PUBLIC KEY-----';
         const publicKey: Forge.pki.rsa.PublicKey = Forge.pki.publicKeyFromPem(pem);
-        const result: string = Buffer.from(publicKey.encrypt(str, "RSAES-PKCS1-V1_5"), "ascii").toString("base64");
+        const result: string = Buffer.from(publicKey.encrypt(message, "RSAES-PKCS1-V1_5"), "ascii").toString("base64");
         return result;
     }
     /**
@@ -94,7 +89,7 @@ tQIDAQAB
      * 
      * @returns token
      */
-    private async login(): Promise<string> {
+    private async getToken(): Promise<string> {
         const url: string = this.url[this.region] + this.url.path[0];
         const data: any = {
             "orgcode": await this.getSchoolCode(),
@@ -126,7 +121,7 @@ tQIDAQAB
      * @returns time
      */
     public async check(): Promise<string> {
-        const token: string = await this.login();
+        const token: string = await this.getToken();
         const url: string = this.url[this.region] + this.url.path[1];
         const data: any = {
             "rspns01": "1",
